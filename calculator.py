0e776e035c0d431a5e70c9b627fedea03c9747c3
Francisco <packo_linares275@hotmail.com>
2015-05-07 15:47:07 -0500

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,16 +1,34 @@
+# -*- coding: utf-8 -*-
 """
-Your module documentation here
+Calculates the sum of the numbers
 """
 
 
-class CalculatorClass(object):
+class calculator_class:
     """
-    Your class documentation here
+    This class contains the method that calculates the sum of the
+    numbers
     """
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        this method calculates the sum of numbers
         """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        # it is instantiated the class calculator_class and sent to
+        # call the method sum
+        suma = 0  # this variable stores the sum of the numbers
+        # This loop realize the sum of the numbers
+        for valor in num_list:
+            suma += valor
+        print suma
+
+        num_list = []  # the numbers are added
+        respuesta = 1  # control variable while loop
+# In this loop numbers are added to the list called numbers
+while respuesta == 1:
+    num_list = [2, 4, 5, 6, 7]
+    num_list.append(num_list)
+    respuesta = ('+'.join(num_list))
+
+calculadora = calculator_class()
+calculadora.sum(num_list)
--- primes.py
+++ primes.py
@@ -1,16 +1,37 @@
+# -*- coding: utf-8 -*-
 """
-Your module documentation here
+Receibe a number and check if it is a prime number
 """
 
 
-class PrimeClass(object):
+class Prime_class:
     """
-    Your class documentation here
+    This class contains the method to check if the number that is
+    entered is a prime number
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        the name of te method is_prime
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        # This indicates the number if times a division led to rest 0
+        numero = 0
+        # This loop is divided the number that you enter among all the
+        # numbers that are between 1 and the number entered
+        for valor in range(1, num_int + 1):  # i takes values 1..n
+            if num_int % valor == 0:    # if the remainder of the
+                # division equal to 0 increases 1
+                numero = numero + 1         # The counter is increase
+        # If the result contains the counter is different from
+        # two then it is not real number so print False and if it
+        # does not print
+        if numero != 2:
+            return False
+        else:
+            return True
+
+# This variable contains the number is entered if it is a real
+# number ignores all number after the decimal point
+        num_int = 11
+# this class is instantiatd prime_clase and sent to call the method
+# is_prime
